1601441fd2883fe146b66e82681486f8eb0829e5
potrdev <164783738+potrdev@users.noreply.github.com>
2024-05-09 15:18:26 +0200

    new change

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import pygame
 
-# Init
-display = pygame.display.set_mode((500,500))
-pygame.display.set_caption("Igra")
+from random import randint
 
 #Settings
+pygame.display.set_caption("Igra")
+FPS = 60
+X = 650
+Y = 400
+
+
+display = pygame.display.set_mode((X,Y))
+
+#Circle
+circle_x = randint(1, X)
+circle_y = randint(1 ,Y)
 
 #Barve
 BLACK = (0,0,0)
@@ -15,8 +24,24 @@ exit = False
 while not exit:
     # Display
     display.fill(WHITE)
+
+    #BG
+    bg = pygame.image.load("bg.jpg")
+    display.blit(bg, (0,0))
+
+    #Elements
+    pygame.draw.circle(display, WHITE, (circle_x, circle_y), 25)
+
+    #Input
+    keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
 
+        if keys[pygame.K_SPACE]:
+            circle_x = randint(1,X)
+            circle_y = randint(1,Y)
+            
+
     pygame.display.update()
+    pygame.time.Clock().tick(FPS)
