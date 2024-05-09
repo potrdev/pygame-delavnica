d17529159ecf8e6bff43fd55bc0c9960040ee4ea
potrdev <164783738+potrdev@users.noreply.github.com>
2024-05-09 14:28:02 +0200

    new change

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+import pygame
+
+# Init
+display = pygame.display.set_mode((500,500))
+pygame.display.set_caption("Igra")
+
+#Settings
+
+exit = False
+
+while not exit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit = True
+
+    pygame.display.update()
