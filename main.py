a66a54e6de79f31be45dc3085bca7cfb056c194e
potrdev <164783738+potrdev@users.noreply.github.com>
2024-05-09 16:15:17 +0200

    new change

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import pygame
 
 from random import randint
 
+import time
+
 pygame.init()
 pygame.font.init()
 
 #Settings
 pygame.display.set_caption("Igra")
 FPS = 60
+
 window_X = 650
 window_Y = 400
 X = window_X - 40
@@ -19,6 +22,9 @@ font = pygame.font.SysFont("mont.ttf", 50)
 
 # Props
 score = 0
+clicked = False
+start = time.time()
+delay = 5
 
 #Circle
 circle_x = randint(1, X)
@@ -31,12 +37,19 @@ WHITE = (255, 255, 255)
 exit = False
 
 while not exit:
+    # Check score
+    if score <= -5:
+        break
+
     # Display
     display.fill(WHITE)
 
     #BG
     bg = pygame.image.load("bg.jpg").convert_alpha()
     display.blit(bg, (0,0))
+
+    # Mouse
+    mousepos = pygame.mouse.get_pos()
 
     #Elements
     circle = pygame.draw.circle(display, WHITE, (circle_x, circle_y), 25)
@@ -54,10 +67,19 @@ while not exit:
         if event.type == pygame.MOUSEBUTTONDOWN:
             click = circle.collidepoint(pygame.mouse.get_pos())
             if click == 1:
+                clicked = True
                 circle_x = randint(1,X)
                 circle_y = randint(1,Y)
                 score += 1
-                display.blit(text, (10,10))    
+                display.blit(text, (10,10))
+                clicked = False
 
-    pygame.display.update()
-    pygame.time.Clock().tick(FPS)
+    if time.time() - start >= delay:
+        if clicked == False:
+            circle_x = randint(1,X)
+            circle_y = randint(1,Y)
+            score -= 1
+            display.blit(text, (10,10))                      
+
+            pygame.display.update()
+            pygame.time.Clock().tick(FPS)
