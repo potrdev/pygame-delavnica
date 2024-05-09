9e5eb583840a44ce75d1af6e01cf0b09eb2abeed
potrdev <164783738+potrdev@users.noreply.github.com>
2024-05-09 14:51:33 +0200

    new change

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ pygame.display.set_caption("Igra")
 
 #Settings
 
+#Barve
+BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+
 exit = False
 
 while not exit:
+    # Display
+    display.fill(WHITE)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
